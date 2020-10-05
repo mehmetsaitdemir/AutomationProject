e6af92a5f8544ea1ecc8d265a8f5aa77c8a4ce73
Mehmet Sait DEMİR <demirmsait@gmail.com>
2020-10-05 10:37:43 +0300

    initial commit

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,5 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("Hello class EU3");
+
+        // more changes are
     }
 }
